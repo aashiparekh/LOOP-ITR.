3bd3b471336b069656847e81f668cfb4a2e8136c
aashiparekh <aashiparekh0511@gmail.com>
2025-09-03 17:47:30 +0530

    Add files via upload

--- loop Q19.c
+++ loop Q19.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-void main()
-{int i;
-printf("numbers from 1 to 100 divisible by 5 are:\n");
-
-for(i=1; i<=100; i++)
-{ if(i%5 ==0)
-printf("%d", i);}
-
-return 0;
+#include<conio.h>
+void main() {
+int i;
+printf("enter numbers between 1 to 100 divisible by 5");
+for(i=1; i=100; i++)
+{if(i%5==0)
+{printf("%d", &i);}
+return 0;}
 }
---
+++ loop Q20.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int i, n, sum=0;
+
+printf("enter the numbers from 1 to 100 divisible by 3");
+
+for(i=1; i<=100; i=i+3)
+{sum=sum + i;}
+
+printf("sum of the numbers from 1 to 100 divisible by 3", i);
+return 0;
+}
